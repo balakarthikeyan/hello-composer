7be972de7f9e477c6ce20d335b386385996e0695
Balakarthikeyan <balakarthikeyan07@gmail.com>
2020-06-23 11:25:51 +0530

    Packages inside SRC

---
+++ src/HelloPackage/SayPackage.php
@@ -0,0 +1,10 @@
+<?php 
+namespace balakarthikeyan\HelloComposer\HelloPackage;
+
+class SayPackage
+{
+    public static function world()
+    {
+        return 'Hello Package, Composer!';
+    }
+}
